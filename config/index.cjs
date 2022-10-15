2912618ffc5799cbff1653bbeb11ec975c05b685
RanXinN <115883822+RanXinN@users.noreply.github.com>
2022-10-15 22:39:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '单小朋友',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oPQh_6R4BMrXrbS9L-efrY_uo2YI',
+      id: 'oPQh_6fzJZ9msm41gcTCNN6wqHSM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '7eTvUZ0P4S2DR0zsABIyJuHumlSSHqZoFsz2oN5Wwys',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
